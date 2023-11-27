d93ed31b47135e0676976d061bd740796f66fb4d
Tamo <tamo@meilisearch.com>
2023-11-27 01:10:20 +0100

    get rid of the two useless color function

--- src/main.rs
+++ src/main.rs
@@ -153,32 +153,3 @@ pub fn u32_to_color(n: u32) -> Color {
 
     Color::from_rgb(r as u8, g as u8, b as u8)
 }
-
-pub fn unrgb(n: u32) -> (u32, u32, u32) {
-    ((n >> 16) & 0xff, (n >> 8) & 0xff, (n) & 0xff)
-}
-
-pub fn hue_to_rgb(hue: f32, saturation: f32, value: f32) -> u32 {
-    assert!((0.0..=360.0).contains(&hue), "bad hue: {}", hue);
-    assert!(
-        (0.0..=1.0).contains(&saturation),
-        "bad saturation: {}",
-        saturation
-    );
-    assert!((0.0..=1.0).contains(&value), "bad value: {}", value);
-
-    let c: f32 = saturation * value;
-    let x: f32 = c * (1.0 - ((hue / 60.0) % 2.0 - 1.0).abs());
-    let m: f32 = value - c;
-    let (r, g, b) = match hue as u32 {
-        0..=59 | 360 => (c, x, 0.0),
-        60..=119 => (x, c, 0.0),
-        120..=179 => (0.0, c, x),
-        180..=239 => (0.0, x, c),
-        240..=299 => (x, 0.0, c),
-        300..=359 => (c, 0.0, x),
-        _ => panic!("called with wrong value for hue"),
-    };
-    let (r, g, b) = ((r + m) * 255.0, (g + m) * 255.0, (b + m) * 255.0);
-    ((r as u32) << 16) | ((g as u32) << 8) | (b as u32)
-}
